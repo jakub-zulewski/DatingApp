b57fff4c8d7fe4d47ce61708aeb400998c8c66fb
jakub-zulewski <j.zulewski@outlook.com>
2023-02-23 17:23:48 +0100

    Initial client commit.

--- API/Program.cs
+++ API/Program.cs
@@ -6,9 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(policy =>
+    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
-
 app.Run();
